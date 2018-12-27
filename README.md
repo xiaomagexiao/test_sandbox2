781b8e1d79f1902aec304c447b35f5b3a61a3df7
xiaomagexiao <1583884738@qq.com>
2018-12-27 15:08:54 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# test_sandbox2
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSasdfdsfsdfsdf我要开直播了dsfsdfddsfdsndbox</h1>
+      <h2>
+        Start editing to 文扬在吗？在啊 dddddddssddddddsdfsdfdsfsdfdsfdsd ddddd
+        ddddd ddddddds see some ssssmagic happen!
+      </h2>
+    </div>
+  );
+}
+//
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
